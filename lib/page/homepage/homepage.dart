a002a4c60df5174a88bff42dab577b058c43a24e
seonbeom <rhtmdqja120@gmail.comn>
2024-04-08 00:54:30 +0900

    홈페이지

---
+++ lib/page/homepage/homepage.dart
@@ -0,0 +1,72 @@
+import 'package:capstone/controller/NavgationBarcontroller.dart';
+import 'package:capstone/page/homepage/chatpage.dart';
+import 'package:capstone/page/homepage/mainpage.dart';
+import 'package:capstone/page/homepage/mypage.dart';
+import 'package:capstone/wiget/bottom.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  final HomeController controller = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
+    // Container의 너비와 높이를 동일하게 설정합니다.
+    final containerSize = screenWidth;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leadingWidth: 80,
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text('최신순'),
+              SizedBox(width: 5), // 아이콘과 텍스트 사이 여백 조절
+              Image.asset('assets/icons/Expand Arrow.png') // 추가할 아이콘
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // 맨 오른쪽에 위치한 첫 번째 아이콘의 클릭 이벤트 처리
+              },
+              icon: Icon(Icons.search), // 맨 오른쪽 첫 번째 아이콘
+            ),
+            IconButton(
+              onPressed: () {
+                // 맨 오른쪽에 위치한 두 번째 아이콘의 클릭 이벤트 처리
+              },
+              icon: Icon(Icons.notifications), // 맨 오른쪽 두 번째 아이콘
+            ),
+            IconButton(
+              onPressed: () {
+                // 맨 오른쪽에 위치한 세 번째 아이콘의 클릭 이벤트 처리
+              },
+              icon: Icon(Icons.settings), // 맨 오른쪽 세 번째 아이콘
+            ),
+          ],
+        ),
+        body: Obx(() {
+          switch (controller.selectedIndex.value) {
+            case 0:
+              return MainPage();
+            case 1:
+              return ChatPage();
+            case 2:
+              return Mypage();
+            default:
+              return MainPage();
+          }
+        }),
+        bottomNavigationBar: CustomBottomNavigationBar(),
+      ),
+    );
+  }
+}
---
+++ lib/wiget/bottom.dart
@@ -0,0 +1,45 @@
+// bottom_navigation_bar.dart
+
+import 'package:capstone/controller/NavgationBarcontroller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final HomeController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => BottomNavigationBar(
+        currentIndex: controller.selectedIndex.value,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              controller.goToHomePage();
+              break;
+            case 1:
+              controller.goToChatPage();
+              break;
+            case 2:
+              controller.goToMyPage();
+              break;
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: '거래채팅',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: '풀잎이',
+          ),
+        ],
+      ),
+    );
+  }
+}
