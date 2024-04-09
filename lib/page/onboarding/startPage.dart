8cd0f2af9012bcbed0638ad0a2bcbabda93ee20d
9aeun <rlarkdms0424@naver.com>
2024-04-09 15:54:17 +0900

    김

--- lib/page/onboarding/startPage.dart
+++ lib/page/onboarding/startPage.dart
@@ -1,8 +1,7 @@
 import 'package:capstone/component/button.dart';
 import 'package:capstone/page/domainPage/domainPage.dart';
-import 'package:capstone/page/domainpage/domainpage.dart';
 import 'package:capstone/page/homepage/homepage.dart';
-import 'package:capstone/page/onboarding/loginpage.dart';
+import 'package:capstone/page/onboarding/Loginpage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:get/get.dart';
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -4,6 +4,8 @@ import 'package:capstone/page/onboarding/loginpage.dart';
 import 'package:get/get.dart';
 import 'package:capstone/page/homePage/homePage.dart'; // 클래스 이름을 대문자로 변경
 import 'package:capstone/page/domainPage/domainPage.dart'; // 클래스 이름을 대문자로 변경
+import 'package:capstone/page/domainPage/DomainPage.dart';
+import 'package:capstone/page/domainPage/LoginPage.dart';
 
 class Routes {
   static final routes = [
