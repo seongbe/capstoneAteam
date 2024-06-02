b66ed9dd125ade74080e8d63e9fef70bd2b6c043
MinseoK1m <pushseo.kim@gmail.com>
2024-06-03 02:07:51 +0900

    관리자 페이지 수정

--- lib/page/domainpage/contact_page_end.dart
+++ lib/page/domainpage/contact_page_end.dart
@@ -1,34 +1,8 @@
 import 'package:capstone/component/contact_container_BLUE.dart';
-import 'package:capstone/page/domainpage/contact_detail_end.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-class ContactPageAllList extends StatelessWidget {
-  const ContactPageAllList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contact Pages'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return ContactPageEnd();
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import "contact_detail_wait.dart";
 
 class ContactPageEnd extends StatelessWidget {
   const ContactPageEnd({Key? key}) : super(key: key);
@@ -36,30 +10,41 @@ class ContactPageEnd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                      onTap: () {
-                        Get.to(ContactDetailEnd());
-                      },
-                  child: ContactContainer_BLUE(
-                    inquiryName: '이런 경우는 어떻게 해야하나요?',
-                    inquiryType: '기타 문의',
-                    id: '2024123123',
-                  ),
-                  );
-                }
-            ),
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('ContactTest').where('state', isEqualTo: true).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+              String inquiryName = data['inquiry_name'];
+              String inquiryType = data['inquiry_type'];
+              String id = data['user_id'];
+              String date = data['date'];
+
+              return GestureDetector(
+                onTap: () {
+                  Get.to(ContactDetailWait());
+                },
+                child: ContactContainer_BLUE(
+                  inquiryName: inquiryName,
+                  inquiryType: inquiryType,
+                  id: id,
+                  date: date,
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
 }
-
