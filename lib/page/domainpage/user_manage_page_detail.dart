08314b493c2f67e1417301cb205c5f35e2dc6812
seonbeom <rhtmdqja120@gmail.comn>
2024-06-02 16:35:06 +0900

    사진연동

--- lib/page/domainpage/user_manage_page_detail.dart
+++ lib/page/domainpage/user_manage_page_detail.dart
@@ -21,6 +21,7 @@ class _UserManagePageDetailState extends State<UserManagePageDetail> {
   late String nickname = '';
   late bool accountStatus = false;
   late String formattedCreatedAt = '';
+  late String profileImageUrl = '';
 
   @override
   void initState() {
@@ -41,142 +42,88 @@ class _UserManagePageDetailState extends State<UserManagePageDetail> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20),
-            Row(
-              children: [
-                SizedBox(width: 20), // 왼쪽 여백 추가
-                Text(
-                  '이메일: ',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)), // 폰트 크기 조정
-                ),
-                SizedBox(width: 10),
-                Text(
-                  '${widget.userEmail}',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                SizedBox(width: 20), // 왼쪽 여백 추가
-                Text(
-                  '닉네임: ',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  '$nickname',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                SizedBox(width: 20), // 왼쪽 여백 추가
-                Text(
-                  '계정 상태: ',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  '${accountStatus ? '활성화' : '정지'}',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                SizedBox(width: 20), // 왼쪽 여백 추가
-                Text(
-                  '계정 생성일시: ',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  '$formattedCreatedAt',
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontFamily: 'skybori',
-                      color: Color(0xFF464646)),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
+            // 프로필 이미지 추가
             Center(
-              child: GreenButton(
-                text1: accountStatus ? '정지하기' : '활성화하기',
-                width: 288,
-                height: 55,
-                onPressed: () {
-                  String action = accountStatus ? '정지' : '활성화';
-                  String alertMessage = '해당 사용자을(를)\n$action 하시겠습니까?';
-                  CustomDialog3.showConfirmationDialog(
-                    context,
-                    alertMessage.replaceFirst('해당 사용자', nickname),
-                    () async {
-                      try {
-                        await FirebaseFirestore.instance
-                            .collection('UserDomainTest')
-                            .where('user_id', isEqualTo: widget.userEmail)
-                            .get()
-                            .then((querySnapshot) {
-                          String docId = querySnapshot.docs.first.id;
-                          FirebaseFirestore.instance
-                              .collection('UserDomainTest')
-                              .doc(docId)
-                              .update({'status': !accountStatus}).then((_) {
-                            Navigator.of(context).pop();
-                            String updatedStatus = accountStatus ? '정지' : '활성화';
-                            CustomDialog.showAlert(
-                              context,
-                              '$nickname의 계정이\n$updatedStatus 되었습니다.',
-                              25,
-                              Colors.black,
-                              () async {
-                                Get.to(UserManagePage());
-                              },
-                            );
-                            // 데이터 새로 고침
-                            fetchUserData();
-                          });
-                        });
-                      } catch (e) {
-                        print('Error updating account status: $e');
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text('계정 상태를 업데이트하는 도중 오류가 발생했습니다.'),
-                        ));
-                      }
-                    },
-                  );
-                },
+              child: Container(
+                padding: EdgeInsets.all(5), // 테두리의 두께를 조정합니다
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xffD0E4BC), // 동그라미의 배경색과 동일하게 설정합니다
+                  border: Border.all(
+                    color: Color(0xffD0E4BC), // 테두리의 색상을 설정합니다
+                    width: 0.5, // 테두리의 두께를 조정합니다
+                  ),
+                ),
+                child: CircleAvatar(
+                  radius: 120,
+                  backgroundImage: profileImageUrl.isNotEmpty
+                      ? NetworkImage(profileImageUrl)
+                      : AssetImage('assets/default_profile.png')
+                          as ImageProvider,
+                  backgroundColor: Colors.grey[200],
+                ),
               ),
+            ),
+
+            SizedBox(height: 80),
+
+            // 상태를 사용하는 위젯을 빌드 메서드에 추가
+            UserDataWidget(
+              nickname: nickname,
+              accountStatus: accountStatus,
+              formattedCreatedAt: formattedCreatedAt,
+              userEmail: widget.userEmail,
+              onPressed: _updateAccountStatus,
             ),
           ],
         ),
       ),
+    );
+  }
+
+  // 상태 업데이트 함수
+  void _updateAccountStatus() async {
+    String action = accountStatus ? '정지' : '활성화';
+    String alertMessage = '해당 사용자을(를)\n$action 하시겠습니까?';
+    CustomDialog3.showConfirmationDialog(
+      context,
+      alertMessage.replaceFirst('해당 사용자', nickname),
+      () async {
+        try {
+          await FirebaseFirestore.instance
+              .collection('UserDomainTest')
+              .where('user_id', isEqualTo: widget.userEmail)
+              .get()
+              .then((querySnapshot) {
+            String docId = querySnapshot.docs.first.id;
+            FirebaseFirestore.instance
+                .collection('UserDomainTest')
+                .doc(docId)
+                .update({'status': !accountStatus}).then((_) {
+              Navigator.of(context).pop();
+              String updatedStatus = accountStatus ? '정지' : '활성화';
+              setState(() {
+                // 상태 업데이트 후에 Alert 메시지를 표시
+                CustomDialog.showAlert(
+                  context,
+                  '$nickname의 계정이\n$updatedStatus 되었습니다.',
+                  25,
+                  Colors.black,
+                  () async {
+                    // 상태 업데이트 후에 UserManagePage로 이동
+                    Get.to(UserManagePage());
+                  },
+                );
+              });
+            });
+          });
+        } catch (e) {
+          print('Error updating account status: $e');
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('계정 상태를 업데이트하는 도중 오류가 발생했습니다.'),
+          ));
+        }
+      },
     );
   }
 
@@ -187,34 +134,145 @@ class _UserManagePageDetailState extends State<UserManagePageDetail> {
           .where('user_id', isEqualTo: widget.userEmail)
           .get();
 
-      // 가져온 데이터 사용하기
       if (querySnapshot.docs.isNotEmpty) {
-        // 첫 번째 문서의 데이터 사용
         var userData = querySnapshot.docs.first.data() as Map<String, dynamic>;
 
-        // 사용자 정보 가져오기
         String fetchedNickname = userData['nickname'] ?? 'N/A';
         bool fetchedAccountStatus = userData['status'] ?? false;
         String fetchedCreatedAt = userData['created_at'] ?? 'N/A';
+        String fetchedProfileImageUrl = userData['profile_image_url'] ??
+            'https://firebasestorage.googleapis.com/v0/b/sku-market-5910e.appspot.com/o/profileImages%2F2024-05-31%2002:08:55.252352.jpg?alt=media&token=eb1f6766-0f7d-4195-b8fa-f2bb2cd7db4b';
 
-        // 계정 생성일시 문자열을 DateTime으로 파싱하고 포맷
         DateTime createdAtDateTime = DateTime.parse(fetchedCreatedAt);
         String formattedFetchedCreatedAt =
             DateFormat('yyyy-MM-dd HH:mm:ss').format(createdAtDateTime);
 
-        // 사용자 정보 업데이트 및 UI 업데이트
         setState(() {
           nickname = fetchedNickname;
           accountStatus = fetchedAccountStatus;
           formattedCreatedAt = formattedFetchedCreatedAt;
+          profileImageUrl = fetchedProfileImageUrl;
         });
       } else {
-        // 사용자 데이터가 없을 경우에 대한 처리
         print('No user data found for user with email: ${widget.userEmail}');
       }
     } catch (e) {
-      // 오류 처리
       print('Error fetching user data: $e');
     }
+  }
+}
+
+// 상태를 사용하는 위젯을 별도의 StatefulWidget으로 분리
+class UserDataWidget extends StatefulWidget {
+  final String nickname;
+  final bool accountStatus;
+  final String formattedCreatedAt;
+  final String userEmail;
+  final VoidCallback onPressed;
+
+  const UserDataWidget({
+    Key? key,
+    required this.nickname,
+    required this.accountStatus,
+    required this.formattedCreatedAt,
+    required this.userEmail,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  _UserDataWidgetState createState() => _UserDataWidgetState();
+}
+
+class _UserDataWidgetState extends State<UserDataWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            SizedBox(width: 20),
+            Text(
+              '이메일: ',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '${widget.userEmail}',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            SizedBox(width: 20),
+            Text(
+              '닉네임: ',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '${widget.nickname}',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            SizedBox(width: 20),
+            Text(
+              '계정 상태: ',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '${widget.accountStatus ? '활성화' : '정지'}',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            SizedBox(width: 20),
+            Text(
+              '계정 생성일시: ',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '${widget.formattedCreatedAt}',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: 'skybori',
+                  color: Color(0xFF464646)),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+      ],
+    );
   }
 }
--- lib/page/homepage/mainpage.dart
+++ lib/page/homepage/mainpage.dart
@@ -1,15 +1,68 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:capstone/page/homepage/postwritepage.dart';
 import 'package:capstone/wiget/BookListItem.dart';
 import 'package:capstone/wiget/mainpost.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
 
-class MainPage extends StatelessWidget {
+class _MainPageState extends State<MainPage> {
+  final ImagePicker picker = ImagePicker();
+  File? selectedImage;
   String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final FirebaseFirestore firebaseFirestore =
+      FirebaseFirestore.instance; // Firestore 인스턴스 추가
+  String? imageUrl; // 다운로드 URL을 저장할 변수
+
+  Future<void> pickImageFromGallery() async {
+    var image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        selectedImage = File(image.path);
+      });
+    }
+  }
+
+  Future<void> uploadImage() async {
+    if (selectedImage == null) return;
+
+    try {
+      final now = DateTime.now();
+      var ref = storage.ref().child('Images/${now.toIso8601String()}.jpg');
+      var uploadTask = ref.putFile(selectedImage!);
+
+      final snapshot = await uploadTask.whenComplete(() => {});
+      final url = await snapshot.ref.getDownloadURL();
+
+      // Firestore에 URL 저장
+      await firebaseFirestore.collection('Images').doc().set({
+        'url': url,
+        'uploaded_at': now,
+      });
+
+      setState(() {
+        imageUrl = url; // 다운로드 URL을 상태로 설정
+      });
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Image uploaded successfully')));
+    } catch (e) {
+      print('Error uploading image: $e');
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error uploading image')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,41 +77,103 @@ class MainPage extends StatelessWidget {
           child: Icon(Icons.add),
         ),
         body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('Book').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (!snapshot.hasData || snapshot.data == null) {
-              return Center(child: Text('데이터 끌어오는거 실패해서 오류난거임'));
-            }
+            stream: FirebaseFirestore.instance.collection('Book').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
 
-            final books = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                final book = books[index];
-                return Column(
-                  children: [
-                    Text(book['imagepath']),
-                    Text(book['title']),
-                    Text(book['subtitle1']),
-                    Text(book['subtitle2']),
-                    BookListItem(
-                      imagePath: 'assets/images/book.png', // 이미지 경로
-                      title: book['title'], // 제목
-                      subtitle1: book['subtitle1'], // 부제목1
-                      subtitle2: book['subtitle2'], // 부제목2
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
+              final books = snapshot.data!.docs;
+
+              return StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection('Images').snapshots(),
+                builder: (context, imageSnapshot) {
+                  if (imageSnapshot.hasError) {
+                    return Center(child: Text('Error: ${imageSnapshot.error}'));
+                  }
+                  if (imageSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  return ListView.builder(
+                    itemCount: books.length,
+                    itemBuilder: (context, index) {
+                      final book = books[index];
+                      return Column(
+                        children: [
+                          BookListItem(
+                            imagePath: 'assets/images/book.png', // 이미지 경로
+                            title: book['title'], // 제목
+                            subtitle1: book['subtitle1'], // 부제목1
+                            subtitle2: book['subtitle2'], // 부제목2
+                          ),
+                          selectedImage != null
+                              ? Image.file(
+                                  selectedImage!,
+                                  width: 110,
+                                  height: 110,
+                                )
+                              : Container(
+                                  width: 110,
+                                  height: 110,
+                                  color: Colors.black,
+                                  child: const Center(
+                                    child: Icon(
+                                      // 선택 안되면 보여줄 아이콘부분
+                                      Icons.image_not_supported,
+                                      color: Colors.white,
+                                      size: 100,
+                                    ),
+                                  ),
+                                ),
+                          ElevatedButton(
+                            onPressed: pickImageFromGallery,
+                            child: Text('Pick Image'),
+                          ),
+                          ElevatedButton(
+                            onPressed: uploadImage,
+                            child: Text('Upload Image'),
+                          ),
+                          StreamBuilder<QuerySnapshot>(
+                              stream: firebaseFirestore
+                                  .collection('Images')
+                                  .snapshots(),
+                              builder: (context, snapshot) {
+                                return (snapshot.connectionState ==
+                                        ConnectionState.waiting)
+                                    ? const Center(
+                                        child: CircularProgressIndicator
+                                            .adaptive(),
+                                      )
+                                    : GridView.builder(
+                                        shrinkWrap: true,
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        gridDelegate:
+                                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                                mainAxisSpacing: 5.0,
+                                                crossAxisSpacing: 5.0,
+                                                crossAxisCount: 3),
+                                        itemCount: snapshot.data!.docs.length,
+                                        itemBuilder: (context, index) {
+                                          final url =
+                                              snapshot.data!.docs[index]['url'];
+                                          return Image.network(
+                                            url,
+                                            width: double.infinity,
+                                            fit: BoxFit.cover,
+                                          );
+                                        });
+                              }),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
+            }),
       ),
     );
   }
