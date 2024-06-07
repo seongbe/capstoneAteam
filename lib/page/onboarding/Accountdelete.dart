16c9f681745d9d152b4dee4ac8b7ce0dd3b18bf2
9aeun <rlarkdms0424@naver.com>
2024-06-07 22:13:29 +0900

    ..

--- lib/page/onboarding/Accountdelete.dart
+++ lib/page/onboarding/Accountdelete.dart
@@ -88,7 +88,7 @@ class _CreatAccountState extends State<CreatAccount> {
         "회원가입이 완료되었습니다.",
         20,
         Colors.black,
-            () {Get.offAll(() => HomePage());},
+            () {Get.offAll(() => HomePage(0));},
       );
     } catch (e) {
       print('Firestore 저장 중 오류 발생: $e');
